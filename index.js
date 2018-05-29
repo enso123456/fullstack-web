23cee7f315f6c365725147df59d4a872a875ecbe
Romeo Enso <enso123456@gmail.com>
2018-05-29 15:44:00 +0800

    file structure route, services

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('./config/keys')
+require('./services/passport')
 
+mongoose.connect(keys.mongoURI)
+ 
 const app = express()
 
-passport.use(
-	new GoogleStrategy(
-	{
-		clientID: keys.googleClientId,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, 
-	(accessToken, refreshToken, profile, done) => {
-		//from the google callback response access token
-		console.log('access token', accessToken)
-		console.log('refresh token', refreshToken)
-		console.log('profile', profile)
-	})
-)
-
-app.get('/auth/google', passport.authenticate('google', {
-	scope: ['profile', 'email']
-}))
-
-// handle this incoming request
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const port = process.env.PORT || 5000
 
