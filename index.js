eaeaff7d522e50e97da5fd616ed4bdfc505ae653
Romeo Enso <enso123456@gmail.com>
2018-05-29 14:24:30 +0800

    add google auth passport

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
 
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'I love Joy Anne Lyn May Carbonilla'})
-})
+passport.use(
+	new GoogleStrategy(
+	{
+		clientID: keys.googleClientId,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	}, 
+	(accessToken, refreshToken, profile, done) => {
+		//from the google callback response access token
+		console.log('access token', accessToken)
+		console.log('refresh token', refreshToken)
+		console.log('profile', profile)
+	})
+)
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}))
+
+// handle this incoming request
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const port = process.env.PORT || 5000
 
