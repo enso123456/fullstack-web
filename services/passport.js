35c8cf1d26b4a883a05485638919c4d02f2e6c4e
Romeo Enso <enso123456@gmail.com>
2018-06-12 09:29:27 +0800

    passport serialize and deserialize

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,18 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
 const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+	done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+	User.find(id).then(user => {
+		done(null, user)
+	})
+})
 
 passport.use(
 	new GoogleStrategy(
