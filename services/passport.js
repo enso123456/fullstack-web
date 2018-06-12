bcf8da0b6deb19e299dbfa88b8935107e2879866
Romeo Enso <enso123456@gmail.com>
2018-06-12 09:46:55 +0800

    testing auth, fetch current_user

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,7 @@ passport.serializeUser((user, done) => {
 })
 
 passport.deserializeUser((id, done) => {
-	User.find(id).then(user => {
+	User.findById(id).then(user => {
 		done(null, user)
 	})
 })
