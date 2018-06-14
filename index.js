d4620ed5d6d9e1c63d827b51733c25139c8b846a
Romeo Enso <enso123456@gmail.com>
2018-06-14 14:48:32 +0800

    config return key for prod

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port)
