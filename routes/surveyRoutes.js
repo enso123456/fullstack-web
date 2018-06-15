2bf9799c684772a24aa0735fbf852cf675fe2adb
Romeo Enso <enso123456@gmail.com>
2018-06-15 16:28:41 +0800

    add redirect url in survey template, provide custom url to clicking survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,12 @@ const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
 
 module.exports = (app) => {
+
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thanks for voting');
+	})
+
+
 	app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
@@ -33,6 +39,5 @@ module.exports = (app) => {
 		} catch (err) {
 			res.status(422).send(err)
 		}
-		
 	});
 };
