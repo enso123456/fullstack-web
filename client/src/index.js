ebdfbb8349164b18714af410d08bb67fb441ef14
Romeo Enso <enso123456@gmail.com>
2018-06-15 16:18:49 +0800

    async await save mongodb

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,10 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
-
-console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY)
-console.log('STRIPE KEY IS', process.env.NODE_ENV)
 
 ReactDom.render(
 	<Provider store={store}><App /></Provider>
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
 
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredit, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
 		const survey = new Survey({
@@ -21,6 +21,18 @@ module.exports = (app) => {
 
 		//send an email
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
+
+		try  {
+			await mailer.send();
+			await survey.save();
+
+			req.user.credits -= 1;
+			const user = await req.user.save();
+
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err)
+		}
+		
 	});
 };
