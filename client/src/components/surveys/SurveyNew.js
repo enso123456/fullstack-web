93ed9c490e6ee6848d0b8f7efa38c7f3e6875449
Romeo Enso <enso123456@gmail.com>
2018-06-18 11:21:07 +0800

    setup redux-form, attach redux-form in reducer

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+//Show Survey form and Survey Reviews
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+
+	render() {
+		return (
+			<div>
+				<SurveyForm />
+			</div>
+		);
+	}
+}
+
+export default SurveyNew;
