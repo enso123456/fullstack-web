c8d142381f55110dd62779ed827d85eb65cffa83
Romeo Enso <enso123456@gmail.com>
2018-06-20 09:43:04 +0800

    find exact survey user, update record of a survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,35 +1,48 @@
 const _ = require('lodash');
 const { URL } = require('url');
-const Path = require('path-parser');//parse url
+const Path = require('path-parser'); //parse url
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredit = require('../middlewares/requireCredit');
+const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
-const Mailer = require('../services/Mailer');
 
 module.exports = (app) => {
 
-	app.get('/api/surveys/thanks', (req, res) => {
+	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('Thanks for voting');
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
+		const p = new Path('/api/surveys/:surveyId/:choice');
 
-		const events = _.map(req.body, ({ url, email }) => {
-			const pathname = new URL(url).pathname;
-			const p = new Path('/api/surveys/:surveyId/:choice');
-			const match = p.test(pathname);
-
-			if (match) {
-				return {
-					email,
-					surveyId: match.surveyId,
-					choice: match.choice
+		_.chain(req.body)
+			.map(({ url, email }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return { email, surveyId: match.surveyId, choice: match.choice }
 				}
-			}
-		});
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ surveryId, email, choice }) => {
+				Survey.updateOne({
+					_id: surveyId,
+					recipients: {
+						$elemMatch: { email: email, responded: false }
+					}
+				}, {
+					$inc: { [choice]: 1 },
+					$set: { 
+						'recipients.$.responded': true,
+						'lastResponded': new Date()
+					},
+
+				}).exec();
+			})
+			.value();
 	});
 
 	app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
